b49c560c24a4de01857826119210fcc7082bd4bc
bharathraj0643 <bharathraj0643@gmail.com>
2025-05-10 11:05:13 +0530

    database as sqlite

--- myapp/settings.py
+++ myapp/settings.py
@@ -86,12 +86,8 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'blog',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
