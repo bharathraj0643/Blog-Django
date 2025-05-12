29059e3bfa83212334ba0207f9a463f0d970bf2b
bharathraj0643 <bharathraj0643@gmail.com>
2025-05-12 11:48:53 +0530

    whitenoise nostatic

--- myapp/settings.py
+++ myapp/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['localhost','127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -126,10 +127,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "blog/static",
-    BASE_DIR / 'media',
+    "blog/static",
 ]
 
 ### render d
