b8b843056c968ca615e3b49f9ac206890af8fead
bharathraj0643 <bharathraj0643@gmail.com>
2025-05-15 14:51:27 +0530

    revert check +static

--- myapp/urls.py
+++ myapp/urls.py
@@ -19,9 +19,9 @@ from django.urls import path , include, re_path
 from django.conf import settings
 
 from django.views.static import serve
-from django.conf.urls.static import static
+# from django.conf.urls.static import static
 
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+#from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 handler404 = 'myapp.views.pageNotFound'
 
@@ -29,7 +29,9 @@ urlpatterns = [
     path("",include("blog.urls")),
     path('admin/', admin.site.urls),
     
-    # re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    # re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
-]  + static(settings.MEDIA_URL , document_root =settings.MEDIA_ROOT)
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+] 
 #+ staticfiles_urlpatterns()
+
+# + static(settings.MEDIA_URL , document_root =settings.MEDIA_ROOT)
