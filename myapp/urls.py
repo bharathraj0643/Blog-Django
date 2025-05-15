cb9bcc427db5a7fb71e3622a32fa66cc6666f457
bharathraj0643 <bharathraj0643@gmail.com>
2025-05-15 14:48:02 +0530

    check +static append

--- myapp/urls.py
+++ myapp/urls.py
@@ -19,7 +19,7 @@ from django.urls import path , include, re_path
 from django.conf import settings
 
 from django.views.static import serve
-# from django.conf.urls.static import static
+from django.conf.urls.static import static
 
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
@@ -29,9 +29,7 @@ urlpatterns = [
     path("",include("blog.urls")),
     path('admin/', admin.site.urls),
     
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
-] 
+    # re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    # re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+]  + static(settings.MEDIA_URL , document_root =settings.MEDIA_ROOT)
 #+ staticfiles_urlpatterns()
-
-# + static(settings.MEDIA_URL , document_root =settings.MEDIA_ROOT)
