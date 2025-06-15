4b3c48877ce4433a3b85c28195f3946866e6f402
bharathraj0643 <bharathraj0643@gmail.com>
2025-06-15 17:54:25 +0530

    railway

--- myapp/settings.py
+++ myapp/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-^79hnc48h@y_h4u0jlbdz-7#6#t2lo%p#fh7ybi&mcny-71jwv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['localhost','127.0.0.1', '.vercel.app' , ".railway.app"]
 
 
 # Application definition
