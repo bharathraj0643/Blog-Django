3e2b345f099885fb5a9268297dd43cad11217d3d
bharathraj0643 <bharathraj0643@gmail.com>
2025-06-15 14:33:32 +0530

    removed vercel env check

--- myapp/settings.py
+++ myapp/settings.py
@@ -142,16 +142,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR , "staticfiles")
 if DEBUG :
     STATICFILES_DIRS = [
         os.path.join(BASE_DIR , "blog/static")
     ]
-
-# vercel
-if os.environ.get("VERCEL"):
-    STATIC_ROOT = os.path.join(BASE_DIR , "staticfiles")
+else:
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-# vercel
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
