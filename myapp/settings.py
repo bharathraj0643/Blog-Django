c17fbe03727f63a15c6e266925ec7df37e7b20b5
bharathraj0643 <bharathraj0643@gmail.com>
2025-06-15 15:47:55 +0530

    Refactor static file handling in settings and update Vercel configuration

--- myapp/settings.py
+++ myapp/settings.py
@@ -147,8 +147,8 @@ if DEBUG :
     STATICFILES_DIRS = [
         os.path.join(BASE_DIR , "blog/static")
     ]
-else:
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
