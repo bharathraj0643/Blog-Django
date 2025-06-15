d84bf8bec9c2110612e0eca85b23f7c7b3d408b0
bharathraj0643 <bharathraj0643@gmail.com>
2025-06-15 19:29:17 +0530

    PyMySQL

--- myapp/settings.py
+++ myapp/settings.py
@@ -15,6 +15,9 @@ import os
 from dotenv import load_dotenv
 import dj_database_url
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
