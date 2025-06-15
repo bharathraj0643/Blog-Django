5888e6d9f7c9260fa0095849cc13f3b76f5128f5
bharathraj0643 <bharathraj0643@gmail.com>
2025-06-15 18:15:37 +0530

    rename

--- myapp/settings.py
+++ myapp/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-^79hnc48h@y_h4u0jlbdz-7#6#t2lo%p#fh7ybi&mcny-71jwv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1', '.vercel.app' , 'https://blog-django-production-382a.up.railway.app']
+ALLOWED_HOSTS = ['localhost','127.0.0.1', '.vercel.app' , 'blog-django-production-382a.up.railway.app']
 
 
 # Application definition
