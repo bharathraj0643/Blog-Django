02f653947a939dbf7f8a59e4f643bf0102d795a4
Bharath Raj S <bharathraj0643@gmail.com>
2025-06-19 19:53:26 +0530

    refactor three files related to collectstatic and ci/cd

--- myapp/settings.py
+++ myapp/settings.py
@@ -157,10 +157,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR , "static")
-if DEBUG :
-    STATICFILES_DIRS = [
-        BASE_DIR / "blog/static"
-    ]
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR , "blog/static")]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
